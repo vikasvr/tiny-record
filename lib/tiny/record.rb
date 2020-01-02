d0cd6c5a8280454e97c3ada5c0ea269ccf45eb69
Vikas Verma <vikasvr23@gmail.com>
2020-01-02 22:23:11 +0530

    support stringified query in fetch where

--- lib/tiny/record.rb
+++ lib/tiny/record.rb
@@ -13,17 +13,20 @@ module TinyRecord
       _get_records(_primary_lookup(id), with).take!
     end
 
-    def fetch_by(columns = {})
-      with = columns.delete(:with)
-      _get_records(columns, with).take
+    def fetch_by(tiny_columns = {})
+      with = tiny_columns.delete(:with)
+      _get_records(tiny_columns, with).take
     end
 
-    def fetch_where(columns = {})
-      with = columns.delete(:with)
-      _get_records(columns, with)
+    def fetch_where(*args)
+      if args[0].is_a?(Hash)
+        _fetch_from_hash(args)
+      else
+        _fetch_from_string(args)
+      end
     end
 
-    def _get_records(by_columns, with_columns)
+    def _get_records(by_columns, with_columns = nil)
       collection = where(by_columns)
       collection = _retrieve_selective_columns(collection,with_columns)
       collection
@@ -33,6 +36,16 @@ module TinyRecord
       target_columns = requested_columns && requested_columns.present? ? requested_columns : _default_tiny_columns
       collection = collection.select(target_columns) if collection.present?
       collection
+    end
+
+    def _fetch_from_hash(args)
+      target_parameter = args[0]
+      with = target_parameter.delete(:with)
+      _get_records(target_parameter, with)
+    end
+
+    def _fetch_from_string(args)
+      _get_records(args)
     end
 
     def _default_tiny_columns
